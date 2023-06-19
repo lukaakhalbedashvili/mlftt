d30abe505accbaef7b6ef88d3fbbdb06bc1acdf3
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2023-06-19 18:26:45 +0400

    feat ( split algorithm )

--- src/components/AnotherTaskSection/index.tsx
+++ src/components/AnotherTaskSection/index.tsx
@@ -10,14 +10,54 @@ const AnotherTaskSection = () => {
     text;
 
     start() {
-      sendTextToAudioRequest(this.text);
+      const maxLength = 230;
+      const minLength = 150;
+
+      let textToWorkOn = this.text;
+      let textToSendArray = [];
+
+      const sliceAndSet = (rangeToSearch: string, signOfSplit: string) => {
+        textToSendArray.push(
+          textToWorkOn.slice(
+            0,
+            rangeToSearch.indexOf(signOfSplit) + minLength + 1
+          )
+        );
+
+        textToWorkOn = textToWorkOn.slice(
+          rangeToSearch.indexOf(signOfSplit) + minLength + 1,
+          textToWorkOn.length
+        );
+      };
+
+      while (textToWorkOn.length > 0) {
+        const rangeToSearch = textToWorkOn.slice(minLength, maxLength);
+
+        if (textToWorkOn.length <= minLength) {
+          textToSendArray.push(textToWorkOn);
+          textToWorkOn = "";
+        } else if (rangeToSearch.includes(".")) {
+          sliceAndSet(rangeToSearch, ".");
+        } else if (rangeToSearch.includes("!")) {
+          sliceAndSet(rangeToSearch, "!");
+        } else if (rangeToSearch.includes("?")) {
+          sliceAndSet(rangeToSearch, "?");
+        } else if (rangeToSearch.includes(";")) {
+          sliceAndSet(rangeToSearch, ";");
+        } else if (rangeToSearch.includes(",")) {
+          sliceAndSet(rangeToSearch, ";");
+        } else if (rangeToSearch.includes(" ")) {
+          sliceAndSet(rangeToSearch, ";");
+        }
+      }
+      console.log(textToSendArray);
     }
 
     onResult() {}
   }
 
   const getAudio = new GetAudio(
-    "გამარჯობა როგორ ხართ დაგიკავშირდით ჩვენს სერვისს მადლობა"
+    "ალექსანდრე სარაჯიშვილის მსგავსი მოსაზრება შედარებით ადრე გამოთქმული აქვს ასევე ალექსანდრე ნანეიშვილსაც[2]. საპირისპიროს ამტკიცებდა რუსთველოლოგი დავით კარიჭაშვილი. მისი აზრით, ვეფხისტყაოსნის პროლოგიცა და ეპილოგიც გაყალბებულია და არანაირი საერთო არა აქვთ პოემის მთავარ ნაწილთან. დავით კარიჭაშვილის მოსაზრებით, ეს ორი ნაწილი ცალ-ცალკეც ერთიანს არაფერს წარმოადგენენ და მათი ცალკეული ტაეპები სხვადასხვა დროს სხვადასხვა პირთაგანაა დაწერილი[3]. დავით კარიჭაშვილის ეს მოსაზრება მკაცრად გააკრიტიკა ნიკოლოზ მარმა თავის რუსთველოლოგიურ ნაშრომში[4]. დავით კარიჭაშვილმა, მიუხედავად ნიკოლოზ მარის კრიტიკისა, საკუთარი მოსაზრებანი უცვლელად გაიმეორა სპეციალურ ნარკვევში „ვეფხისტყაოსნის შემადგენლობა“[5]. ეს მოსაზრებები გაიზიარა და კიდევ უფრო განავითარა რუსთველოლოგმა ალექსანდრე სვანიძემ[6]. დავით კარიჭაშვილს ერთი პერიოდი მხარს უჭერდა"
   );
 
   useEffect(() => {
